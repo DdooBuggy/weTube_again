59a5ef84d3eefd400d87076b8edeca661701b7fc
BuggyDdoo <31423595+DdooBuggy@users.noreply.github.com>
2022-03-11 13:36:32 +0900

    ~#5

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,8 +1,37 @@
-export const trending = (req, res) => res.render("home");
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: "first video",
+      rating: 4,
+      comments: 3,
+      createdAt: "2 min ago",
+      views: 20,
+      id: 1,
+    },
+    {
+      title: "second video",
+      rating: 4,
+      comments: 90,
+      createdAt: "26 min ago",
+      views: 10,
+      id: 2,
+    },
+    {
+      title: "third video",
+      rating: 3,
+      comments: 7,
+      createdAt: "34 min ago",
+      views: 45,
+      id: 3,
+    },
+  ];
+  return res.render("home", { pageTitle: "Home", videos });
+};
 export const searchVideo = (req, res) => res.send("Search Videos");
 export const upload = (req, res) => res.send("Upload video");
-export const watch = (req, res) => res.render("watch");
-export const editVideo = (req, res) => res.render("edit");
+export const watch = (req, res) => res.render("watch", { pageTitle: "Watch" });
+export const editVideo = (req, res) =>
+  res.render("edit", { pageTitle: "Edit" });
 export const deleteVideo = (req, res) => {
   return res.send(`Delete Video: ${req.params.id}`);
 };
