73c7ccbd5e367272058d8dd117d153b60fd9a9ff
BuggyDdoo <31423595+DdooBuggy@users.noreply.github.com>
2022-03-18 12:08:00 +0900

    ~#9.7 webpack

--- src/server.js
+++ src/server.js
@@ -30,6 +30,7 @@ app.use(localsMiddleware);
 
 // routers
 app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("assets"));
 app.use("/", rootRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
